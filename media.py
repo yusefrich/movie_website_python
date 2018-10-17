32d458ce161dc6cf38c02bafab6f98cb5a85d31a
yusefrich <yusef.rick@gmail.com>
2018-10-17 19:39:15 -0300

    primeira correcao pep8

--- media.py
+++ media.py
@@ -1,19 +1,25 @@
 import webbrowser
 
+
 class Movie():
-    #contructor for the movie
-    #add here any value that is showed in the website
-    def __init__(self, movie_title, movie_storyline,porter_image, trailer_youtube, movie_imdb_url):
+    # contructor for the movie
+    # add here any value that is showed in the website
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 porter_image,
+                 trailer_youtube,
+                 movie_imdb_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = porter_image
         self.trailer_youtube_url = trailer_youtube
         self.imdb_url = movie_imdb_url
 
-    #shows movie trailer
+    # shows movie trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
-    #shows imdb page
+    # shows imdb page
     def show_imdb_info(self):
         webbrowser.open(self.imdb_url)
